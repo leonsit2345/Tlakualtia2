2d7f7b0e9c3adc53f9a87c0a6bafa8adbfbb930d
leonsit2345 <76213025+leonsit2345@users.noreply.github.com>
2024-10-24 22:38:36 -0400

    Add files via upload

---
+++ Tlakualtia.js
@@ -0,0 +1,12 @@
+function mostrar(){
+    document.getElementById('fotitos').style.display = 'flex';
+    document.getElementById('boton').style.display = 'none';
+    document.getElementById('colegio').style.display = 'none';
+    document.getElementById('redes').style.display = 'none';
+}
+function ocultar(){
+    document.getElementById('fotitos').style.display = 'none';
+    document.getElementById('boton').style.display = 'block';
+    document.getElementById('colegio').style.display = 'block';
+    document.getElementById('redes').style.display = 'block';
+}
